76c4aa77af8d055a4907bedb769497d4bf98a55b
Jatin Oza <ozajatin9309@gmail.com>
2022-07-01 14:22:03 +0530

    Newsletter updated

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
+const https = require("https");
 const port = 3000;
 
 app.use(express.static("public")); // its the folder in which all our static data is stored (local files)
@@ -12,14 +13,50 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-    let firstName = req.body.fname;
-    let lastName = req.body.lnmae;
-    let email = req.body.email;
-    console.log("First name is: " + firstName);
-    console.log("Last name is: " + lastName);
-    console.log("Email is: " + email);
+    const firstName = req.body.fname;
+    const lastName = req.body.lnmae;
+    const email = req.body.email;
+    // console.log("First name is: " + firstName);
+    // console.log("Last name is: " + lastName);
+    // console.log("Email is: " + email);
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_field: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us11.api.mailchimp.com/3.0/lists/c0fa84b6cf";
+
+    const options = {
+        method: "POST",
+        auth: "jatin:9aea50de197cab11f4bfe2dbe7bbe9d5-us11"
+    }
+    const request = https.request(url, options, (response) => {
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port localhost:${port}`);
 })
+
+// API key
+//9aea50de197cab11f4bfe2dbe7bbe9d5-us11
+
+// List Id / Audience id -> it is where we are gonna store our mail subscriber
+//c0fa84b6cf.
