517f0b6c07e03842bc39e66fa0bee61c1a46add3
Jatin Oza <ozajatin9309@gmail.com>
2022-09-14 12:24:10 +0530

    creating social links part

---
+++ portfolio/src/components/SocialLinks.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from "react-icons/hi"
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
+const SocialLinks = () => {
+
+    const links = [
+        {
+            id: 1,
+            child: (
+                <>
+                    LinkedIn  <FaLinkedin size={30} />
+                </>
+            ),
+            href: 'https://linkedin.com',
+            style: 'rounded-tr-md'
+        },
+        {
+            id: 2,
+            child: (
+                <>
+                    Github <FaGithub size={30} />
+                </>
+            ),
+            href: 'https://github.com/jatin-code21',
+        },
+        {
+            id: 3,
+            child: (
+                <>
+                    Mail   <HiOutlineMail size={30} />
+                </>
+            ),
+            href: 'mailto: ozajatin9309@gmail.com',
+        },
+        {
+            id: 4,
+            child: (
+                <>
+                    Resume   <BsFillPersonLinesFill size={30} />
+                </>
+            ),
+            href: '/resume.pdf',
+            style: 'rounded-br-md',
+            download: true
+        }
+    ]
+
+
+    return (
+        <div className='flex flex-col top-[35%] left-0 fixed'>
+            <ul>
+                <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 '>
+                    <a href="" className='flex justify-between item-center w-full text-white'>
+                        <>
+                            LinkedIn <FaLinkedin size={30} />
+                        </>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default SocialLinks
