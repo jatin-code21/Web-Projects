afec20a173edeb4db6044c0c4a1d789d9135a350
Jatin Oza <ozajatin9309@gmail.com>
2022-09-14 13:37:20 +0530

    social link part completed

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -48,9 +48,9 @@ const NavBar = () => {
 
             {nav && (
                 <ul className='flex flex-col justify-center item-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
-                    {links.map(({ id, link }) => {
+                    {links.map(({ id, link }) => (
                         <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'> {link}</li>
-                    })}
+                    ))}
                 </ul>
             )}
 
--- portfolio/src/components/SocialLinks.jsx
+++ portfolio/src/components/SocialLinks.jsx
@@ -49,15 +49,18 @@ const SocialLinks = () => {
 
 
     return (
-        <div className='flex flex-col top-[35%] left-0 fixed'>
+        <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
             <ul>
-                <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 '>
-                    <a href="" className='flex justify-between item-center w-full text-white'>
-                        <>
-                            LinkedIn <FaLinkedin size={30} />
-                        </>
-                    </a>
-                </li>
+
+                {links.map(({id, child, href, style, download}) => (
+                    <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300' + " " + style}>
+                        <a href={href} className='flex justify-between item-center w-full text-white' download={download} target='_blank' rel="noreferrer">
+                            {child}
+                        </a>
+                    </li>
+                ))}
+
+
             </ul>
         </div>
     )
