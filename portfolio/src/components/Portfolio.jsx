5cec3b9bd7a194dc3b3c13d1e9cc02d002cc9aa1
Jatin Oza <ozajatin9309@gmail.com>
2022-10-23 01:36:12 +0530

    all updated

--- portfolio/src/components/Portfolio.jsx
+++ portfolio/src/components/Portfolio.jsx
@@ -3,7 +3,7 @@ import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg"
 import installNode from "../assets/portfolio/installNode.jpg"
 import navbar from "../assets/portfolio/navbar.jpg"
 import reactParallax from "../assets/portfolio/reactParallax.jpg"
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg"
+// import reactSmooth from "../assets/portfolio/reactSmooth.jpg"
 import reactWeather from "../assets/portfolio/reactWeather.jpg"
 
 const Portfolio = () => {
